db6975d62db6f5cf93d086e7b8b49728fa38deac
JuliaP <flaeeee@gmail.com>
2022-12-14 15:54:00 +0300

    работа завершена

--- finalWork/Program.cs
+++ finalWork/Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArray(int[] collection)
+﻿/*void FillArray(int[] collection)
 {
     int length = collection.Length;
     int index = 0;
@@ -46,4 +46,35 @@ int [] CutArray(int [] array)
 int [] array = new int[10];
 FillArray(array);
 PrintArray(array);
-PrintArray(CutArray(array));
+PrintArray(CutArray(array));*/
+
+void ShortArray(string [] array, string [] newArray)
+{
+    int count = 0;
+    for(int i =0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+          newArray[count] = array[i];
+          count++;  
+        }
+    }
+}
+
+void ShowArray (string [] newArray)
+{
+    for(int i = 0; i < newArray.Length; i++)
+    { 
+       Console.Write($"{newArray[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+string [] array = {"hello","1","world","2022","123", "good", "bye","happy","new","year","2023"};
+string [] newArray = new string [array.Length];
+
+ShortArray(array,newArray);
+ShowArray(newArray);
+
+
