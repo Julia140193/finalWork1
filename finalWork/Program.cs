b16f9384949dea0f41189206d02b705748820b70
JuliaP <flaeeee@gmail.com>
2022-12-14 15:56:04 +0300

    работа завершена1

--- finalWork/Program.cs
+++ finalWork/Program.cs
@@ -1,54 +1,4 @@
-﻿/*void FillArray(int[] collection)
-{
-    int length = collection.Length;
-    int index = 0;
-    while(index < length)
-    {
-        collection[index] = new Random().Next(-10, 10);
-        index++;
-    }
-}
-
-void PrintArray(int[] col)
-{
-    int count = col.Length;
-    
-    for(int i = 0; i< count; i++)
-    {
-        Console.Write($"{col[i]} ");
-    }
-    Console.WriteLine();
-}
-
-int [] CutArray(int [] array)
-{
-    int length = 0;
-    if(array.Length > 10)
-    {
-        length = array.Length / 10;
-    }
-    else
-    {
-        if(array.Length / 3 ==1)
-        {
-            length = 3;
-            return array;
-        }
-        else
-        {
-            length--;
-        }
-    }
-    return array;
-}
-
-
-int [] array = new int[10];
-FillArray(array);
-PrintArray(array);
-PrintArray(CutArray(array));*/
-
-void ShortArray(string [] array, string [] newArray)
+﻿/*void ShortArray(string [] array, string [] newArray)
 {
     int count = 0;
     for(int i =0; i < array.Length; i++)
@@ -75,6 +25,6 @@ string [] array = {"hello","1","world","2022","123", "good", "bye","happy","new"
 string [] newArray = new string [array.Length];
 
 ShortArray(array,newArray);
-ShowArray(newArray);
+ShowArray(newArray);*/
 
 
